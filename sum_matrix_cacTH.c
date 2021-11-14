28a3ee5f5358380d5ad887456cc3a01f749c14ac
Zankus <90998182+HaxCy@users.noreply.github.com>
2021-11-14 22:35:11 +0700

    Update and rename sum_matrix_cacTH to sum_matrix_cacTH.c

--- sum_matrix_cacTH.c
+++ sum_matrix_cacTH.c
@@ -15,6 +15,8 @@ void printf_matrix(int matrix[][MAX], int line, int column){
 		printf("\t%d", matrix[i/line][i%line]);
 	}
 }
+
+//tinh tong mang
 void sum_matrix(int matrix[][MAX], int line, int column){
 	int sum=0;
 	for(int i=0; i<line*column; i++){
@@ -22,6 +24,8 @@ void sum_matrix(int matrix[][MAX], int line, int column){
 	}
 	printf("\nTong cac phan tu trong mang la: %d ", sum);
 }
+
+//tinh tonng ma tran voi cot nhap vao
 void sum_column(int matrix[][MAX], int line, int column){
 	int x, sum=0;
 	do{
@@ -33,6 +37,7 @@ void sum_column(int matrix[][MAX], int line, int column){
 	}
 	printf("\nTong cua dong %d la: %d", x, sum);
 }
+//tinh tonng ma tran voi dong nhap vao
 void sum_line(int matrix[][MAX], int line, int column){
 	int x, sum=0;
 	do{
@@ -44,18 +49,24 @@ void sum_line(int matrix[][MAX], int line, int column){
 	}
 	printf("\nTong cua dong %d la: %d", x, sum);
 }
+
+//tinh tong duong cheo chinh va phu
 void sum_cheo(int matrix[][MAX], int line){
 	int sum=0;
 	for(int i=0; i<line; i++){
 		sum+=matrix[i][i];
 	}
 	printf("\nTong duong cheo chinh la: %d", sum);
+	
+	
 	sum=0;
 	for(int i=0; i<line; i++){
 		sum+=matrix[line-i-1][i];
 	}
 	printf("\nTong duong cheo phu la: %d", sum);
 }
+
+//tinh tong tam giac tren va duoi neu do la ma tran vuong
 void sum_triangle_updow(int matrix[][MAX], int line){
 	int sum=0;
 	for(int i=1; i<line; i++){
@@ -81,9 +92,11 @@ int main(){
 	sum_matrix(matrix, line, column);
 	sum_column(matrix, line, column);
 	sum_line(matrix, line, column);
+	
 	if(line==column){
 		sum_cheo(matrix, line);
 		sum_triangle_updow(matrix, line);
 	}
+	
 	return 0;
 }

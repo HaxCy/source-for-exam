666baecc2a528964b807affb043aae8a1dbe32fd
Zankus <90998182+HaxCy@users.noreply.github.com>
2021-11-14 22:44:43 +0700

    Create sapxepmang.c

---
+++ sapxepmang.c
@@ -0,0 +1,27 @@
+int void swap(int *a, int *b){
+	int temp=*a;
+	*a=*b;
+	*b=temp;
+}
+
+//sap xep tang
+void sapxep(int *arr, int n){
+	for(int i=0; i<n; i++){
+		for(int j=i+1; j<n; j++){
+			if(arr[i]>arr[j]){
+				swap(arr+i, arr+j);
+			}
+		}
+	}
+}
+
+//sap xep giam
+void sapxep2(*arr, int n){
+	for(int i=0; i<n; i++){
+		for(int j=i+1; j<n; j++){
+			if(arr[i]<arr[j]){
+				swap(arr+i, arr+j);
+			}
+		}
+	}
+}
